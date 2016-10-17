f99a8fec81c2574a8182fe3c8984e8914be3b41e
Rye Terrell <ryeterrell@ryeterrell.net>
2016-10-17 14:14:42 -0500

    #1: WIP Fixes

--- reactive/elasticsearch.py
+++ reactive/elasticsearch.py
@@ -11,6 +11,9 @@ from charms.reactive import (
 
 from charms import apt
 
+from charmhelpers.core.host import service_restart
+from charmhelpers.core.templating import render
+
 from charmhelpers.core.hookenv import (
     log,
     config,
@@ -21,27 +24,29 @@ from charmhelpers.core.hookenv import (
 
 @when_not('elasticsearch.installed')
 def check_install_path():
+    # Make sure we've got the resource.
     try:
-        status_set('Checking for resources')
+        status_set('maintenance', 'Checking for resources')
         deb = resource_get('deb')
     except Exception:
         message = 'Error fetching the elasticsearch deb resource.'
         log(message)
         status_set('blocked', message)
         return
-
+    # We've got the resource. If it's of an expected size, install it.
     filesize = os.stat(deb).st_size
     if deb and filesize > 1000000:
         set_state('elasticsearch.deb-install')
         return
-
+    # We've got the resource, but it doesn't appear to have downloaded properly.
+    # Attempt apt installing elasticsearch instead.
     set_state('elasticsearch.apt-install')
 
 
 @when('elasticsearch.apt-install')
 @when_not('apt.installed.elasticsearch')
 def apt_install():
-    status_set('Queuing dependencies for install')
+    status_set('maintenance', 'Queuing dependencies for install')
     apt.queue_install(['elasticsearch'])
 
 
@@ -51,7 +56,7 @@ def level_set():
 
 
 @when('elasticsearch.deb-install')
-@when_not('elasticsearch.install')
+@when_not('elasticsearch.installed')
 def deb_install():
     deb = resource_get('deb')
     d = DebPackage(deb)
@@ -62,9 +67,28 @@ def deb_install():
 @when('elasticsearch.installed')
 @when_not('elasticsearch.configured')
 def configure_elasticsearch():
+    status_set('maintenance', 'Configuring elasticsearch')
+    # render(
+    #     source = "elasticsearch.yml",
+    #     target = "/etc/elasticsearch/elasticsearch.yml",
+    #     owner = "root",
+    #     perms = 0o644,
+    #     context = {'config': hookenv.config()}
+    # )
     set_state('elasticsearch.configured')
+    set_state('elasticsearch.restart')
 
 
 @when('config.changed')
 def reconfigure():
+    status_set('maintenance', 'Configuring elasticsearch')
     remove_state('elasticsearch.configured')
+    set_state('elasticsearch.restart')
+
+
+@when('elasticsearch.restart')
+def restart():
+    status_set('maintenance', 'Restarting elasticsearch')
+    service_restart('elasticsearch')
+    remove_state('elasticsearch.restart')
+    status_set('active', 'Ready')
