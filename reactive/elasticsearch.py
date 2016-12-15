650df0bbfafbdf07dd63f7d70b8bc62cb4b26465
sebastienpattyn93 <sebastien.pattyn@gmail.com>
2016-12-15 10:53:36 +0100

    Charm now supports online and offline install

--- reactive/elasticsearch.py
+++ reactive/elasticsearch.py
@@ -1,19 +1,21 @@
+#!/usr/bin/env python3
+# pylint: disable=c0111,c0103,c0301
 import os
+import subprocess
 
+from subprocess import CalledProcessError
 from apt.debfile import DebPackage
-
+from jujubigdata import utils
+from charms import apt
 from charms.reactive import (
+    hook,
     when,
     when_not,
     set_state,
     remove_state,
 )
-
-from charms import apt
-
+from charmhelpers.core import hookenv, templating
 from charmhelpers.core.host import service_restart
-from charmhelpers.core.templating import render
-
 from charmhelpers.core.hookenv import (
     log,
     config,
@@ -22,7 +24,70 @@ from charmhelpers.core.hookenv import (
 )
 
 
+@when('java.installed')
 @when_not('elasticsearch.installed')
+def preinstall():
+    conf = config()
+    install_package = conf['install-package']
+    if install_package == 'offline':
+        check_install_path()
+    else:
+        set_state('elasticsearch.apt-install')
+
+@when('elasticsearch.apt-install', 'java.installed')
+@when_not('apt.installed.elasticsearch')
+def apt_install():
+    status_set('maintenance', 'Queuing dependencies for install')
+    apt.queue_install(['elasticsearch'])
+
+@when('apt.installed.elasticsearch', 'java.installed')
+def level_set():
+    set_state('elasticsearch.installed')
+
+@when('elasticsearch.deb-install', 'java.installed')
+@when_not('elasticsearch.installed')
+def deb_install():
+    try:
+        deb = resource_get('deb')
+        d = DebPackage(deb)
+        d.install()
+        set_state('elasticsearch.installed')
+    except CalledProcessError:
+        status_set('error', 'Elasticsearch could not be installed with package')
+
+@when('elasticsearch.installed', 'java.installed')
+@when_not('elasticsearch.configured')
+def configure_elasticsearch():
+    status_set('maintenance', 'Configuring elasticsearch')
+    utils.re_edit_in_place('/etc/elasticsearch/elasticsearch.yml', {
+        r'#network.host: 192.168.0.1': 'network.host: ["_site_", "_local_"]',
+    })
+    set_state('elasticsearch.configured')
+
+@when('config-changed')
+def reconfigure():
+    status_set('maintenance', 'Configuring elasticsearch')
+    set_state('elasticsearch.configured')
+
+@when('elasticsearch.configured', 'java.installed')
+def restart():
+    try:
+        status_set('maintenance', 'Restarting elasticsearch')
+        service_restart('elasticsearch')
+        set_state('elasticsearch.ready')
+        status_set('active', 'Ready')
+    except CalledProcessError:
+        status_set('error', 'Could not restart elasticsearch')
+
+
+@when('client.connected')
+def connect_to_client(client):
+    conf = config()
+    cluster_name = conf['cluster-name']
+    port = conf['port']
+    client.configure(port, cluster_name)
+
+
 def check_install_path():
     # Make sure we've got the resource.
     try:
@@ -38,57 +103,7 @@ def check_install_path():
     if deb and filesize > 1000000:
         set_state('elasticsearch.deb-install')
         return
-    # We've got the resource, but it doesn't appear to have downloaded properly.
-    # Attempt apt installing elasticsearch instead.
-    set_state('elasticsearch.apt-install')
-
-
-@when('elasticsearch.apt-install')
-@when_not('apt.installed.elasticsearch')
-def apt_install():
-    status_set('maintenance', 'Queuing dependencies for install')
-    apt.queue_install(['elasticsearch'])
-
-
-@when('apt.installed.elasticsearch')
-def level_set():
-    set_state('elasticsearch.installed')
-
-
-@when('elasticsearch.deb-install')
-@when_not('elasticsearch.installed')
-def deb_install():
-    deb = resource_get('deb')
-    d = DebPackage(deb)
-    d.install()
-    set_state('elasticsearch.installed')
-
-
-@when('elasticsearch.installed')
-@when_not('elasticsearch.configured')
-def configure_elasticsearch():
-    status_set('maintenance', 'Configuring elasticsearch')
-    # render(
-    #     source = "elasticsearch.yml",
-    #     target = "/etc/elasticsearch/elasticsearch.yml",
-    #     owner = "root",
-    #     perms = 0o644,
-    #     context = {'config': hookenv.config()}
-    # )
-    set_state('elasticsearch.configured')
-    set_state('elasticsearch.restart')
-
-
-@when('config.changed')
-def reconfigure():
-    status_set('maintenance', 'Configuring elasticsearch')
-    remove_state('elasticsearch.configured')
-    set_state('elasticsearch.restart')
-
-
-@when('elasticsearch.restart')
-def restart():
-    status_set('maintenance', 'Restarting elasticsearch')
-    service_restart('elasticsearch')
-    remove_state('elasticsearch.restart')
-    status_set('active', 'Ready')
+    else:
+            # We've got the resource, but it doesn't appear to have downloaded properly.
+            # Attempt apt installing elasticsearch instead.
+        set_state('elasticsearch.apt-install')
